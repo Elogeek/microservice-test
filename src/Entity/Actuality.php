077e480bb1df63aedd5b5f155881760986c25049
elogeek <elodiechristin@gmail.com>
2022-05-14 17:03:57 +0200

    start project

---
+++ src/Entity/Actuality.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Entity;
+
+use App\Repository\ActualityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ActualityRepository::class)]
+class Actuality {
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'varchar')]
+    private ?string $title;
+
+    #[ORM\Column(type: 'varchar')]
+    private ?string $content;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string {
+        return $this->title;
+    }
+
+    /**
+     * @param string|null $title
+     * @return Actuality
+     */
+    public function setTitle(?string $title): self {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getContent(): ?string {
+        return $this->content;
+    }
+
+    /**
+     * @param string|null $content
+     * @return Actuality
+     */
+    public function setContent(?string $content): self {
+        $this->content = $content;
+        return $this;
+    }
+
+}
